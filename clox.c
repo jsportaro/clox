0900d720446d539fae7ead1915aca9c12e138163
Joseph Portaro <jsportaro@outlook.com>
2024-09-04 13:50:56 -0400

    Added VM with decode/dispatch for returns and printing constants (15.1.1)

--- clox.c
+++ clox.c
@@ -1,20 +1,25 @@
 #include <common.h>
 #include <chunk.h>
 #include <debug.h>
+#include <vm.h>
 
 int main(int argc, const char* argv[]) {
     UNUSED(argc);
     UNUSED(argv);
 
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_RETURN, 123);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "TEST CHUNK");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
