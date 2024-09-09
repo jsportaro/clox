084f9879ec63e376067ced067cd8bd277940b0c5
Joseph Portaro <jsportaro@outlook.com>
2024-09-08 21:18:11 -0400

    Added compiler support for define variables

--- compiler.c
+++ compiler.c
@@ -165,6 +165,10 @@ static uint8_t parseVariable(const char* errorMessage) {
     return identifierConstanct(&parser.previous);
 }
 
+static void defineVariable(uint8_t global) {
+    emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static void binary() {
     TokenType operatorType = parser.previous.type;
     ParseRule* rule = getRule(operatorType);
