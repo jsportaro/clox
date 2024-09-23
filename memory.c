110317d471ba88bb6548c6daa0019ab8d094df80
Joseph Portaro <jsportaro@outlook.com>
2024-09-22 22:05:17 -0400

    Added method declarations (28.1.3)

--- memory.c
+++ memory.c
@@ -89,6 +89,7 @@ static void blackenObject(Obj* object) {
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
+            markTable(&klass->methods);
             break;
         }
         case OBJ_CLOSURE: {
@@ -127,6 +128,8 @@ static void freeObject(Obj* object) {
 
     switch (object->type) {
         case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            freeTable(&klass->methods);
             FREE(ObjClass, object);
             break;
         }
